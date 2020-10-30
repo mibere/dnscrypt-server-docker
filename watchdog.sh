e93b73532fe3b127193c9e7542cddf4e08dbddc3
mibere <mibere@users.noreply.github.com>
2020-10-30 15:51:35 +0100

    Redis

--- watchdog.sh
+++ watchdog.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
-sleep 300
+sleep 180
+
+for service in redis-server; do
+    service "$service" status || service "$service" --full-restart
+done
 
 for service in unbound encrypted-dns; do
     sv check "$service" || sv force-restart "$service"
